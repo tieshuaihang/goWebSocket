3e69e8a95cc71ab3629f9d1dd15cdad04d57c7e6
tieshuaihang <1063880806@qq.com>
2022-03-29 20:50:26 +0800

    完成WebSocket握手

--- server.go
+++ server.go
@@ -1,9 +1,44 @@
 package main
 
-import "net/http"
+import (
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
+var (
+	upgrader = websocket.Upgrader{
+		// 允许跨域
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello websocket"))
+	var (
+		conn *websocket.Conn
+		err  error
+		data []byte
+	)
+
+	// Upgrader:websocket  完成HTTP响应及握手
+	if conn, err = upgrader.Upgrade(w, r, nil); err != nil {
+		return
+	}
+
+	// websocket.Conn  消息类型有 Text、Binary 不过一般用Json
+	for {
+		if _, data, err = conn.ReadMessage(); err != nil {
+			goto ERR
+		}
+		if err = conn.WriteMessage(websocket.TextMessage, data); err != nil {
+			goto ERR
+		}
+	}
+
+ERR:
+	conn.Close()
+
 }
 
 func main() {
